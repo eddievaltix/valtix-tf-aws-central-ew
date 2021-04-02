d7a7d8c2eae607d5a4a97316ec1936431ca73c78
Eddie Tang <eddie@valtix.com>
2021-04-02 04:01:28 +0000

    added ingress rule

--- valtix_policy/rule.tf
+++ valtix_policy/rule.tf
@@ -12,14 +12,6 @@ resource "valtix_policy_rule_set" egress_policy_rule_set {
 
 resource "valtix_policy_rule_set" ingress_policy_rule_set {
   name = var.ingress_policy_ruleset_name
-  rule {
-    name        = "rule1"
-    description = "placeholder rule for forwarding https"
-    type        = "Forwarding"
-    action      = "ALLOW_LOG"
-    service     = valtix_service_object.forward-https.service_id
-    state       = "DISABLED"
-  }
 }
 
 resource "valtix_policy_rule_set" ew_policy_rule_set {
